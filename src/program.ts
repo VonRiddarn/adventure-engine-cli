4700fc0b6df2295da4e5fad76d6673157df647b1
VonRiddarn <vonriddarn@gmail.com>
2025-09-13 13:40:11 +0200

    feat: implement test loop with enforced user input handling

--- src/program.ts
+++ src/program.ts
@@ -1,1 +1,18 @@
+import { getEnforcedInput } from "./managers/input-manager.js";
+
 console.log("Hello world");
+
+let isRunning: boolean = true;
+
+const startGame = async () => {
+	while (isRunning) {
+		console.log("Entered menu.");
+		const a = await getEnforcedInput("What would you like to do?", { validLength: 1 });
+		console.log("Got valid input.");
+		if (a === "1") isRunning = false;
+	}
+
+	console.log("Bye.");
+};
+
+await startGame();
