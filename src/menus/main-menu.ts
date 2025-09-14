52cd9ab49d3c378a1bd6ce12c5fd63bc4574febd
VonRiddarn <vonriddarn@gmail.com>
2025-09-14 18:47:04 +0200

    feat: implement MainMenu and integrate it into startGame function

---
+++ src/menus/main-menu.ts
@@ -0,0 +1,24 @@
+import type { MenuOption } from "../types/menu-option.js";
+import type { Menu } from "../types/menu.js";
+
+const MainMenu = (): Menu => {
+	const options: MenuOption[] = [
+		{ key: "1", label: "Option 1", action: () => {} },
+		{ key: "2", label: "Option 2", action: () => {} },
+		{ key: "3", label: "Option 3", action: () => {} },
+		{ key: "4", label: "Option 3", action: () => {} },
+	];
+
+	const enter = () => {
+		console.log("Oh, guess I'm in!");
+	};
+
+	const update = (): boolean => {
+		console.log("Nah, Imma head out.");
+		return false;
+	};
+
+	return { enter, update };
+};
+
+export default MainMenu;
--- src/program.ts
+++ src/program.ts
@@ -1,24 +1,13 @@
 import { getEnforcedInput } from "./managers/input-manager.js";
+import MainMenu from "./menus/main-menu.js";
+import { enterMenu } from "./utilities/menu-utilities.js";
 
 console.log("Hello world");
 
-let isRunning: boolean = true;
-
 const startGame = async () => {
-	while (isRunning) {
-		console.log("Entered menu.");
-		const a = await getEnforcedInput("What would you like to do?", [
-			{ key: "1", label: "Option 1", action: () => {} },
-			{ key: "2", label: "Option 2", action: () => {} },
-			{ key: "3", label: "Option 3", action: () => {} },
-			{ key: "4", label: "Option 3", action: () => {} },
-		]);
-
-		console.log("Got valid input.");
-		if (a === "4") isRunning = false;
-	}
+	enterMenu(MainMenu());
 
 	console.log("Bye.");
 };
 
-await startGame();
+startGame();
