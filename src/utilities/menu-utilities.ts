b534be0e401d1a9971569d8ae9b41d69629f9b17
VonRiddarn <vonriddarn@gmail.com>
2025-09-15 01:44:05 +0200

    feat: make use of the renderers newFrame method inside enterMenu from menu-utilities.

--- src/utilities/menu-utilities.ts
+++ src/utilities/menu-utilities.ts
@@ -1,13 +1,14 @@
 import type { Menu } from "../types/menu.js";
+import * as renderer from "../utilities/renderer.js";
 /**
  * Enters a menu and locks the program in an update loop until the update returns false.
  */
 export const enterMenu = async (menu: Menu) => {
-	// renderer.newFrame();
+	renderer.newFrame();
 	menu.enter();
 	while (await menu.update()) {
 		// TODO: Add cleanup here so that menus arent responsible of UI clearing.
-		// renderer.newFrame();
+		renderer.newFrame();
 	}
 	menu.exit();
 };
