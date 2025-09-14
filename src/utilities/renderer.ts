9590b79d0741e79fe9ef69f02a03ecfb102eb94d
VonRiddarn <vonriddarn@gmail.com>
2025-09-15 01:27:38 +0200

    feat: implement formatOption and listOptions functions for menu option rendering

---
+++ src/utilities/renderer.ts
@@ -0,0 +1,9 @@
+import type { MenuOption } from "../types/menu-option.js";
+
+export const formatOption = ({ key, label }: MenuOption) => `[${key}] ${label}`;
+
+export const listOptions = (options: MenuOption[]) => {
+	options.forEach((o) => {
+		console.log(formatOption(o));
+	});
+};
